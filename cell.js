1ae34b476d184611b1ae41eaf854edceabb9fe8a
spartanglidarn <erikbostrom1@gmail.com>
2017-07-14 00:43:06 +0200

    Add neighbours to the cells

--- cell.js
+++ cell.js
@@ -1,24 +1,76 @@
-var Cell = function(positionX, positionY, isAlive, neighbours){
+function Cell (positionX, positionY, isAlive, board){
 
-	var posX = positionX;
-	var posY = positionY;
-	var isAlive = isAlive;
+	//set Cell attributes
+	this.posX = positionX;
+	this.posY = positionY;
+	this.isAlive = isAlive;
 
-	function returnCellPosition(){
-		var position = {posX: this.posX, posY: this.posY};
-		return position;
-	};
+	this.neighbours = [];
+	this.neighbours.push.apply(this.neighbours, findNeighbours(this.posX, this.posY, board));
 
-	function killCell(){
-		this.isAlive = false;
-	};
-
-	function reviveCell(){
-		this.isAlive = true;
-	};
-
-	function findCellNeighbours(){
-		return //all neighbours
-	};
 }
 
+function findNeighbours(posX, posY, board){
+
+	var neighbours = [];
+	//collect all directions in array so we can loop them when getting positions of neighbours
+	var directions = ["N", "NE", "E", "SE", "S", "SW", "W", "NW"];
+	
+	//loop all the directions and check at what position every neighbour is at
+	for(var x = 0; x < directions.length; x++){
+		
+		//the neighbours position is returnd as a array with posX and posY (array insteed of object for easier check of length)
+		var neighbourPositions = getNeighbourPosition(posX, posY, board, directions[x])
+		
+		//only return legit neighbours
+		if (neighbourPositions != undefined){
+			neighbours.push(neighbourPositions);
+		}		
+	}
+
+	return neighbours;
+
+}
+
+//direction can be one of the following N, NE, E, SE, S, SW, W, NW
+function getNeighbourPosition(posX, posY, board, direction){
+
+	var neighbourPosY = posY;
+	var neighbourPosX = posX;
+
+	switch(direction){
+		case "N": 
+			neighbourPosY = posY - 1;
+			break;
+		case "NE":
+			neighbourPosX = posX + 1;
+			neighbourPosY = posY - 0;
+			break;
+		case "E":
+			neighbourPosX = posX + 1;
+			break;
+		case "SE":
+			neighbourPosX = posX + 1;
+			neighbourPosY = posY + 1;
+			break;
+		case "S":
+			neighbourPosY = posY + 1;
+			break;
+		case "SW":
+			neighbourPosX = posX - 1;
+			neighbourPosY = posY + 1;
+			break;
+		case "W":
+			neighbourPosX = posX - 1;
+			break;
+		case "NW":
+			neighbourPosX = posX - 1;
+			neighbourPosY = posY - 1;
+	}
+
+	if(((neighbourPosY && neighbourPosX) > 0) && ((neighbourPosY && neighbourPosX) <= board.yAxis)){
+		position = {posX: neighbourPosX, posY: neighbourPosY};
+		return position;
+	}
+
+}
--- main.js
+++ main.js
@@ -1,21 +1,37 @@
-//this is where the game logic is executed
-var counter = 0;
-var cells = [];
-var bord = {xAxis:10, yAxis:10};
-//main loop that will loop and execute the game logic every time
-//start with a 60 fps and see how it preforms
-while (counter < 1/* any cells are allice*/){
-	
+function mainFunction(){
+
+	//this is where the game logic is executed
+	var counter = 0;
+	var cells = [];
+	var board = {xAxis:10, yAxis:10};
+
 	//creating all the cells
-	for(var x = 0; x < 10; x++){
-		for (var y = 0; y < 10; y++){
-			cells.push(new Cell(x, y, false, "test"))
+	for(var x = 0; x < board['xAxis']; x++){
+		for (var y = 0; y < board.yAxis; y++){
+			cells.push(new Cell(x, y, false, board))
 		}
 	}
-	var test = cells[1];
-	console.log(test.returnCellPosition());
 
-	counter ++;
-	console.log("inside main game loop");
+	//main loop that will loop and execute the game logic every time
+	//start with a 60 fps and see how it preforms
+	//while (counter < 100/* any cells are allice*/){
+	for(var xy = 0; xy < cells.length; xy++){	
+		var test = cells[xy];
+		console.log("position x: " + test.posX);
+		console.log("position y: " + test.posY);
+
+		for (var xyz = 0; xyz < test.neighbours.length; xyz++){
+			console.log("		neighbours: " + test.neighbours[xyz]);
+		}
+		console.log(" ");
+		counter ++;
+		
+	}
 }
 
+//run the js code after the document is loaded to the DOM
+var tid = setInterval( function () {
+    if ( document.readyState !== 'complete' ) return;;
+    clearInterval( tid );       
+    mainFunction();
+}, 100 );
