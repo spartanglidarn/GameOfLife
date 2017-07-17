a48955fb45d4d6890b44a6f42ca05e2b89b31663
spartanglidarn <erikbostrom1@gmail.com>
2017-07-18 00:38:58 +0200

    finding all neighbours to one cell

--- cell.js
+++ cell.js
@@ -1,36 +1,34 @@
-function Cell (positionX, positionY, isAlive, board){
+function _cell (positionX, positionY, isAlive){
 
 	//set Cell attributes
 	this.posX = positionX;
 	this.posY = positionY;
 	this.isAlive = isAlive;
+	this.neighbours = [],
 
-	this.neighbours = [];
-	this.neighbours.push.apply(this.neighbours, findNeighbours(this.posX, this.posY, board));
+	//create function to find the Cell's neighbours
+	this.findNeighbours = function (board){
+		//collect all directions in array so we can loop them when getting positions of neighbours
+		var directions = ["N", "NE", "E", "SE", "S", "SW", "W", "NW"];
 
-}
+		//loop all the directions and check at what position every neighbour is at
+		var neighbourCount = 0;
+		for(var x = 0; x < directions.length; x++){
+			//the neighbours position is returnd as a Object with posX and posY 
+			var neighbourPositions = getNeighbourPosition(this.posX, this.posY, board, directions[x]);
+			
+			//only return legit neighbours
+			if (neighbourPositions != undefined){
+				console.log("	posX: " + neighbourPositions.posX + " posY: " + neighbourPositions.posY);
+				neighbourCount ++;
+			};	
 
-function findNeighbours(posX, posY, board){
+		};
+		this.neighbours.push(neighbourCount);	
+	};
+};
 
-	var neighbours = [];
-	//collect all directions in array so we can loop them when getting positions of neighbours
-	var directions = ["N", "NE", "E", "SE", "S", "SW", "W", "NW"];
-	
-	//loop all the directions and check at what position every neighbour is at
-	for(var x = 0; x < directions.length; x++){
-		
-		//the neighbours position is returnd as a array with posX and posY (array insteed of object for easier check of length)
-		var neighbourPositions = getNeighbourPosition(posX, posY, board, directions[x])
-		
-		//only return legit neighbours
-		if (neighbourPositions != undefined){
-			neighbours.push(neighbourPositions);
-		}		
-	}
 
-	return neighbours;
-
-}
 
 //direction can be one of the following N, NE, E, SE, S, SW, W, NW
 function getNeighbourPosition(posX, posY, board, direction){
@@ -38,13 +36,14 @@ function getNeighbourPosition(posX, posY, board, direction){
 	var neighbourPosY = posY;
 	var neighbourPosX = posX;
 
+
 	switch(direction){
 		case "N": 
 			neighbourPosY = posY - 1;
 			break;
 		case "NE":
 			neighbourPosX = posX + 1;
-			neighbourPosY = posY - 0;
+			neighbourPosY = posY - 1;
 			break;
 		case "E":
 			neighbourPosX = posX + 1;
@@ -68,9 +67,11 @@ function getNeighbourPosition(posX, posY, board, direction){
 			neighbourPosY = posY - 1;
 	}
 
-	if(((neighbourPosY && neighbourPosX) > 0) && ((neighbourPosY && neighbourPosX) <= board.yAxis)){
-		position = {posX: neighbourPosX, posY: neighbourPosY};
-		return position;
+	if(neighbourPosX >=0 && neighbourPosX <= (board.xAxis - 1)){
+		if (neighbourPosY >= 0 && neighbourPosY <= (board.yAxis - 1)){
+			var position = {posX: neighbourPosX, posY: neighbourPosY};
+			return position;
+		}
 	}
 
 }
--- main.js
+++ main.js
@@ -3,20 +3,21 @@ function mainFunction(){
 	//this is where the game logic is executed
 	var counter = 0;
 	var cells = [];
-	var board = {xAxis:10, yAxis:10};
+	var board = {xAxis:5, yAxis:5};
 
 	//creating all the cells
-	for(var x = 0; x < board['xAxis']; x++){
+	for(var x = 0; x < board.xAxis; x++){
 		for (var y = 0; y < board.yAxis; y++){
-			cells.push(new Cell(x, y, false, board))
+			cells.push(new _cell(x, y, false))
 		}
 	}
 
 	//main loop that will loop and execute the game logic every time
 	//start with a 60 fps and see how it preforms
-	//while (counter < 100/* any cells are allice*/){
+	//while (/* any cells are allice*/){
 	for(var xy = 0; xy < cells.length; xy++){	
 		var test = cells[xy];
+		test.findNeighbours(board);
 		console.log("position x: " + test.posX);
 		console.log("position y: " + test.posY);
 
